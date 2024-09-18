5392bb4aabffedae229abc8abba67189bebf76c6
Logan <lcagle101@gmail.com>
2024-09-18 16:47:09 -0500

    Adding all changes made to Toms branch. (pulled in class Monday 9.16) This includes the ability to use the python tester and some figures. Along with this, I have optimized my Quicksort Algorithm. This file is test_var006.c. These optimizations prove to have sped up the algorithm.

--- sorting-lab-ver01/sorting-lab/plotter.py
+++ sorting-lab-ver01/sorting-lab/plotter.py
@@ -2,7 +2,13 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv('example.csv')
-plt.plot(df['size'], df['throughput'])
+df = pd.read_csv('Some_Opt.csv')
+plt.plot(df['size'], df['throughput'], label = 'Some_Opt')
+df1 = pd.read_csv('Some_Opt_t2.csv')
+plt.plot(df1['size'], df1['throughput'], label = 'Some_Opt_t2')
+df2 = pd.read_csv('QuickSort.csv')
+plt.plot(df2['size'], df2['throughput'], label = 'QuickSort')
+plt.legend()
 plt.show()
+
 
--- sorting-lab-ver01/sorting-lab/test_var005_QS.c
+++ sorting-lab-ver01/sorting-lab/test_var005_QS.c
@@ -1,23 +1,21 @@
+/*
+  This is a simple implementation of an unoptimized sort.
+
+  - richard.m.veras@ou.edu
+
+*/
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #ifndef COMPUTE_NAME
+#define COMPUTE_NAME baseline
 #endif
-
-#define INSERTION_SORT_THRESHOLD 32
-
-void insertionSort(float arr[], int l, int r) {
-    for (int i = l + 1; i <= r; i++) {
-        float key = arr[i];
-        int j = i - 1;
-        while (j >= l && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-}
+void copyArray(float *x, float *y, int n);
+void quickSort(float arr[], int lowBound, int highBound);
+void swap(float *a, float *b);
+int partition(float arr[], int lowBound, int highBound);
 // Function to swap two elements
 void swap(float* a, float* b) {
     float t = *a;
@@ -25,9 +23,19 @@ void swap(float* a, float* b) {
     *b = t;
 }
 
+//Function to copy arrays more efficiently than a for loop
+void copyArray(float *x, float *y, int n) {
+    float *end = x + n; // Pointer to the end of the array x
+    while (x < end) {
+        *y = *x; // Copy the value pointed by x to the location pointed by y
+        x++;     // Move to the next element in x
+        y++;     // Move to the next element in y
+    }
+}
+
 int partition(float arr[], int lowBound, int highBound){
 
-	//obtain original pivot
+	//obtain original piv
 	float piv = arr[highBound];
 
 	int temp = lowBound - 1; //This is the right position of pivot
@@ -48,16 +56,19 @@ void quickSort(float arr[], int lowBound, int highBound){
 	if (lowBound < highBound) {
 		int partitions = partition(arr, lowBound, highBound);
 		// recursively call to go through entire array
+
 		quickSort(arr, lowBound, partitions -1);
 		quickSort(arr, partitions + 1, highBound);
 	}
 
 }
+void COMPUTE_NAME( int m0,
+		   float *x,
+		   float *y )
 
-void COMPUTE_NAME(int m0, float* x, float* y) {
-    // Copy the contents of the input array into the output array
-    memcpy(y, x, m0 * sizeof(float));
+{
+	memcpy(y, x, m0 * sizeof(float));
+	//copyArray(x, y, m0);
+	quickSort(y, 0, m0 -1);
 
-    // Merge sort
-    quickSort(y, 0, m0 -1);
 }
